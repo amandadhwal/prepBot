0375bfaff239bc53b1212161122599caa22c3591
Aman Dadhwal <amand@Amans-MacBook-Air-3.local>
2025-02-15 16:22:03 +0530

    Resolved merge conflict in layout.jsx

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,34 +1,15 @@
-import react from 'react';
+import React from 'react';
 import Header from "./_components/Header";
 
-
-function DashboardLayout({children})
-{
-    return( <div>
-        <Header/>
-<<<<<<< HEAD
-        <div className='mx-5 md:mx-20 lg:mx-36'>
-             {children}
+function DashboardLayout({children}) {
+    return (
+        <div>
+            <Header />
+            <div className='mx-5 md:mx-20 lg:mx-36'>
+                {children}
+            </div>
         </div>
-=======
-<<<<<<< HEAD
-        {children}
->>>>>>> 98a542c (Added AddNewInterview component and updated UI components)
-    </div>
-    )
+    );
 }
-export default DashboardLayout;
-<<<<<<< HEAD
 
-=======
-=======
-        <div className='mx-5 md:mx-20 lg:mx-36'>
-             {children}
-        </div>
-    </div>
-    )
-}
 export default DashboardLayout;
-
->>>>>>> 6df2f99 (pop form complete)
->>>>>>> 98a542c (Added AddNewInterview component and updated UI components)
