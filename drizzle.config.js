2a544bcb1004c5ebd09719af1e6ea360833f37b5
Aman Dadhwal <amand@Amans-MacBook-Air-3.local>
2025-02-16 17:07:40 +0530

    Resolved merge conflicts in drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,18 +1,10 @@
 /** @type {import("drizzle-kit").Config} */
 const config = {
-    schema: './utils/schema.js',
-    dialect: 'postgresql',
-    dbCredentials: {
-<<<<<<< HEAD
-      url: 'postgresql://accounts:npg_IQHlDWqX0fg5@ep-steep-sunset-a525u1mf-pooler.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require'},
-=======
-<<<<<<< HEAD
-      url: 'postgresql://accounts:npg_IQHlDWqX0fg5@ep-steep-sunset-a525u1mf-pooler.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require',
-    },
-=======
-      url: 'postgresql://accounts:npg_IQHlDWqX0fg5@ep-steep-sunset-a525u1mf-pooler.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require'},
->>>>>>> 6df2f99 (pop form complete)
->>>>>>> 98a542c (Added AddNewInterview component and updated UI components)
+      schema: './utils/schema.js',
+      dialect: 'postgresql',
+      dbCredentials: {
+          url: 'postgresql://accounts:npg_IQHlDWqX0fg5@ep-steep-sunset-a525u1mf-pooler.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require',
+      },
   };
   
   export default config;
