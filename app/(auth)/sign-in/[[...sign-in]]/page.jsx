8e49dfa57519c2f2122fe52dbabfb4e0234c5cdc
Aman Dadhwal <amand@Amans-MacBook-Air-3.local>
2025-03-27 00:09:56 +0530

    complete

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -66,8 +66,7 @@ export default function Page() {
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500 dark:text-gray-400">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-            quibusdam aperiam voluptatum.
+            Make your Interview Prepration easy with PrepBot.
           </p>
         </div>
 
